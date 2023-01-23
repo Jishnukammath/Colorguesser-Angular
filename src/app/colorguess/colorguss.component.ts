04241013659ea8da25d9ad7ba37d3f12d1d68795
jishnukammath <jishnukammath1999@gmail.com>
2023-01-23 09:23:08 +0530

    guessingsequence

--- src/app/colorguess/colorguss.component.ts
+++ src/app/colorguess/colorguss.component.ts
@@ -8,18 +8,34 @@ import { Component } from '@angular/core';
 export class ColorgussComponent {
 
 constructor(){}
+ansercolor=""
+msg="";
 color="";
 rgba=""
 randomNumber=""
+randomNumber1=""
+randomNumber2=""
+color1=""
+color2=""
 maxVal = 0xFFFFFF; 
 randColor=""
 colochange(){
+  this.msg=""
 this.color="yellow"
 let randomNumber = Math.random() * this.maxVal; 
-randomNumber = Math.floor(randomNumber);
-this.randomNumber = randomNumber.toString(16);
-this.randColor = this.randomNumber.padStart(6);  
-this.color="#"+this.randColor.toString()
+let randomNumber1 = Math.random() * this.maxVal; 
+let randomNumber2 = Math.random() * this.maxVal; 
+
+this.randomNumber = Math.floor(randomNumber).toString(16);
+this.randomNumber1 = Math.floor(randomNumber1).toString(16);
+this.randomNumber2 = Math.floor(randomNumber2).toString(16);
+
+this.color="#"+this.randomNumber
+this.color1="#"+this.randomNumber1
+this.color2="#"+this.randomNumber2
+
+
+
 
 
 const r = parseInt(this.color.slice(1, 3), 16);
@@ -27,8 +43,21 @@ const g = parseInt(this.color.slice(3, 5), 16);
 const b = parseInt(this.color.slice(5, 7), 16);
 this.rgba=`rgb(${r}, ${g}, ${b})`;
 
+ 
 
+}
 
+firstcolor(colors:any){
+  console.log(colors)
+  console.log(this.color)
 
+if (colors==this.color.toString()){
+  this.ansercolor="green"
+  this.msg="currect anser"
+
+}else{
+  this.ansercolor="red"
+this.msg="wrong anser"
+}
 }
 }
